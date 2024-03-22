ecf716bdfd4ce079112bc11dfa5d2010c97bc1f7
Asheesh Kumar <asheesh220720@gmail.com>
2024-03-22 11:26:15 +0530

    initial changes

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,3 +4,5 @@ const blue = Color.fromARGB(224, 39, 4, 196);
 const fillColor = Colors.grey;
 const backgroundColor = Color.fromARGB(224, 39, 4, 196);
 const white = Colors.white;
+const yellow = Colors.yellow;
+const grey = Color(0xFFEEEEEE);  // shade[200]
